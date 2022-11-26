9c1bb40379519fb06b362c3e91d2069d4a8081db
Kevin M <algarete23@gmail.com>
2022-11-26 13:33:56 -0400

    update middleware to check for valid api keys and update access log

--- src/Http/Middleware/LaravelSimpleApiKeyMiddleware.php
+++ src/Http/Middleware/LaravelSimpleApiKeyMiddleware.php
@@ -4,16 +4,22 @@ namespace Kustomrt\LaravelSimpleApiKey\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Kustomrt\LaravelSimpleApiKey\Models\ApiKey;
 
 class LaravelSimpleApiKeyMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
-        // Perform action
+        // Api Key is missing
+        abort_unless($key = $request->bearerToken(), 401);
 
-        abort_unless($request->bearerToken(), 401);
+        // Check if Api Key is valid
+        $apiKey = ApiKey::isValid($key);
+        abort_unless($apiKey instanceof ApiKey, 403);
 
-        abort(403);
+        $apiKey->update([
+            'last_used_at' => now()
+        ]);
 
         return $next($request);
     }
